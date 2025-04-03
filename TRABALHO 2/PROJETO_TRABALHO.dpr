16aa53ab2814bf75719c4c439d5a71ad05cbe4d7
HenriqueSolivo <henriquesolivo@gmail.com>
2025-04-03 12:08:15 -0300

    Add files via upload

---
+++ TRABALHO 2/PROJETO_TRABALHO.dpr
@@ -0,0 +1,16 @@
+program PROJETO_TRABALHO;
+
+uses
+  Vcl.Forms,
+  TRABALHO in 'TRABALHO.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
